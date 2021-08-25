7763583485a9ef0bf52b2c77c7884b6275d07d9b
dipengfei <pengfei.di@rakuten.com>
2021-08-25 23:17:02 +0800

    initial commit

---
+++ src/main/kotlin/me/danielpf/springplaykt/SpringPlayKtApplication.kt
@@ -0,0 +1,11 @@
+package me.danielpf.springplaykt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringPlayKtApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringPlayKtApplication>(*args)
+}
